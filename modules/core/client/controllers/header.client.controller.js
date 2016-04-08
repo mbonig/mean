6fb477c411db1db14cac37abb0d09d6c58b4b63d
Matthew Bonig <matt.bonig@starz.com>
2016-04-08 15:29:12 -0600

    papafied the header controller

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,26 +1,33 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
-  function ($scope, $state, Authentication, Menus) {
-    // Expose view variables
-    $scope.$state = $state;
-    $scope.authentication = Authentication;
+    angular.module('core').controller('HeaderController', HeaderController);
 
-    // Get the topbar menu
-    $scope.menu = Menus.getMenu('topbar');
+    HeaderController.$inject = ['$scope', '$state', 'Authentication', 'Menus'];
 
-    // Get the account menu
-    $scope.accountMenu = Menus.getMenu('account').items[0];
+    function HeaderController($scope, $state, Authentication, Menus) {
+        var vm = this;
+        // Expose view variables
+        vm.$state = $state;
+        vm.authentication = Authentication;
+        vm.feedbackEmail = "";
+        vm.feedbackSubject = "";
 
-    // Toggle the menu items
-    $scope.isCollapsed = true;
-    $scope.toggleCollapsibleMenu = function () {
-      $scope.isCollapsed = !$scope.isCollapsed;
-    };
+        // Get the topbar menu
+        vm.menu = Menus.getMenu('topbar');
 
-    // Collapsing the menu after navigation
-    $scope.$on('$stateChangeSuccess', function () {
-      $scope.isCollapsed = true;
-    });
-  }
-]);
+        // Get the account menu
+        vm.accountMenu = Menus.getMenu('account').items[0];
+
+        // Toggle the menu items
+        vm.isCollapsed = true;
+        vm.toggleCollapsibleMenu = function () {
+            vm.isCollapsed = !vm.isCollapsed;
+        };
+
+        // Collapsing the menu after navigation
+        $scope.$on('$stateChangeSuccess', function () {
+            vm.isCollapsed = true;
+        });
+    }
+})();
