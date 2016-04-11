3e0772acab8f198de806e6f69e8487bd4512f62f
Matthew Bonig <matt.bonig@starz.com>
2016-04-11 10:17:11 -0600

    feat(config): simplifying the mongoDB config string choices.

--- config/env/production.js
+++ config/env/production.js
@@ -5,7 +5,7 @@ module.exports = {
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/starz-mean',
+    uri: process.env.MONGOURL || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/starz-mean',
     options: {
       user: '',
       pass: ''
